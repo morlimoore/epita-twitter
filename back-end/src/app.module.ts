06d588b38660498c7c13996462bcde60c5da8e2a
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-08-15 09:10:24 +0200

    feat: Add Polls module with complete implementation

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { TweetsModule } from './tweets/tweets.module';
 import { RepliesModule } from './replies/replies.module';
 import { RetweetsModule } from './retweets/retweets.module';
+import { PollsModule } from './polls/polls.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from '@hapi/joi';
 import { AuthModule } from './auth/auth.module';
@@ -26,6 +27,7 @@ import { AuthModule } from './auth/auth.module';
     TweetsModule,
     RepliesModule,
     RetweetsModule,
+    PollsModule,
     AuthModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
