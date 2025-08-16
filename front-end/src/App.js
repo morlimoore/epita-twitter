aa59dc6d238bba57aea97986d483e9a71c76fc97
Riya Kumari <riyakumari@Riyas-MacBook-Air.local>
2025-08-16 11:52:15 +0200

    user profile implemented with overview of tweets and followers and following and into

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,18 @@
 import './styles/App.css';
 import SplashScreen from './pages/SplashScreen';
+import Profile from './pages/Profile';
+import LogoutScreen from './pages/LogoutScreen';
 // import SimpleTest from './SimpleTest';
 
 function App() {
   return (
     <div className="App">
-      <SplashScreen />
+      <Profile />
+      {/* <SplashScreen /> */}
+      {/* <LogoutScreen 
+        onCancel={() => console.log('Cancel logout')}
+        onConfirmLogout={() => console.log('Confirmed logout')}
+      /> */}
     </div>
   );
 }
